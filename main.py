ad849920d38efa5572d34b1a5eedd4d456f6b1db
jjordanoc <joaquin.jordan@utec.edu.pe>
2023-08-26 21:06:45 -0500

    feat: added zero-th point intersection regression

--- main.py
+++ main.py
@@ -2,47 +2,61 @@ import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
+from math import ceil, floor
+
 mpl.use('macosx')
 
 df = pd.read_csv("insurance.csv")
-df.plot.scatter(x="bmi", y="charges")
-df.plot.scatter(x="age", y="charges")
-
-
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-
-ax.scatter(df["age"], df["bmi"], df["charges"], marker='o')
-
-
-ax.set_xlabel('Age')
-ax.set_ylabel('BMI')
-ax.set_zlabel('Charges')
-ax.legend()
-
-
-X = df[["age", "bmi"]].to_numpy()
-rows, cols = X.shape
-ones = np.ones((rows, 1), dtype=X.dtype)
-X = np.append(X, ones, axis=1)
-print(X, X.shape)
 
 y = df["charges"].to_numpy()
 
-XT = np.transpose(X)
+# Calculate the coefficients assuming a zero-th point intersection (no bias)
+A = df[["age", "bmi"]].to_numpy()
+AT = np.transpose(A)
+b = np.matmul(np.linalg.inv(np.matmul(AT, A)), np.matmul(AT, y))
+print(f"The first coefficient assuming a zero-th point intersection is: {b[0]}")
+print(f"The second coefficient assuming a zero-th point intersection is: {b[1]}")
+print(f"Regressor plane: Y={round(b[0], 2)}X1+{round(b[1], 2)}X2")
 
-b = np.matmul(np.linalg.inv(np.matmul(XT, X)), np.matmul(XT, y))
 
-# Define the range of x and y values
-xx1 = np.linspace(-10, 10, 100)
-xx2 = np.linspace(-10, 10, 100)
+# Calculate the bias coefficients not assuming a zero-th point intersection (bias included)
+B = df[["age", "bmi"]].to_numpy()
+rows, cols = B.shape
+ones = np.ones((rows, 1), dtype=B.dtype)
+B = np.append(B, ones, axis=1)
+y = df["charges"].to_numpy()
+BT = np.transpose(B)
+b = np.matmul(np.linalg.inv(np.matmul(BT, B)), np.matmul(BT, y))
+print(f"The first coefficient not assuming a zero-th point intersection is: {b[0]}")
+print(f"The second coefficient not assuming a zero-th point intersection is: {b[1]}")
+print(f"The bias term not assuming a zero-th point intersection is: {b[2]}")
+print(f"Regressor plane: Y={round(b[0], 2)}X1+{round(b[1], 2)}X2+{round(b[2],2)}")
 
+
+# Plot the raw data and a superimposing plane on the data that passes through the origin
+min_age, max_age = floor(min(df["age"])), ceil(max(df["age"]))
+min_bmi, max_bmi = floor(min(df["bmi"])), ceil(max(df["bmi"]))
+xx1, xx2 = np.meshgrid(range(min_age, max_age + 1), range(min_bmi, max_bmi + 1))
 Z = b[0] * xx1 + b[1] * xx2 + b[2]
-
-ax.plot_surface(xx1, xx2, Z, color='b', alpha=0.5)
-
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.set_xlabel('Age')
+ax.set_ylabel('BMI')
+ax.set_zlabel('Charges')
+ax.plot_surface(xx1, xx2, Z, color='r', alpha=0.5)
+ax.scatter(df["age"], df["bmi"], df["charges"], marker='o', color='b')
 plt.show()
 
-# X, Y = np.meshgrid(x, y)
-
-print(b)
+# Plot the raw data and a superimposing plane on the data that does not pass through the origin
+min_age, max_age = floor(min(df["age"])), ceil(max(df["age"]))
+min_bmi, max_bmi = floor(min(df["bmi"])), ceil(max(df["bmi"]))
+xx1, xx2 = np.meshgrid(range(min_age, max_age + 1), range(min_bmi, max_bmi + 1))
+Z = b[0] * xx1 + b[1] * xx2 + b[2]
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.set_xlabel('Age')
+ax.set_ylabel('BMI')
+ax.set_zlabel('Charges')
+ax.plot_surface(xx1, xx2, Z, color='r', alpha=0.5)
+ax.scatter(df["age"], df["bmi"], df["charges"], marker='o', color='b')
+plt.show()
